b197a9703bc6d873ebde3e91e63d2bdd2fa3fe0d
Kyle Qian <cqian@stanford.edu>
2016-06-27 14:11:10 -0700

    Initial commit

---
+++ config/application.rb
@@ -0,0 +1,25 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module GuidesApi
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins 'http://localhost:3000'
+        resource '*',
+        :headers => :any,
+        :methods => [:get, :post, :delete, :put, :options, :head]
+      end
+    end
+    
+  end
+end
