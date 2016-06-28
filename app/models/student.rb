f3de06c65425a3502ec0deb9fbafbc86b46e895a
Kyle Qian <cqian@stanford.edu>
2016-06-28 15:57:28 -0700

    Finished adding instance methods and added has_many through

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,9 +1,18 @@
 class Student < ApplicationRecord
   belongs_to :school
   has_many :student_teacher_pairs
+  has_many :teachers, through: :student_teacher_pairs
   has_one :student_record
 
-  def can_graduate
+  def can_graduate?
     return student_record.credits >= School::CREDITS_FOR_GRADUATION
+  end
+
+  def teachers_who_teach_math
+  	result = []
+  	teachers.each do |t|
+  		result.append(t) if t.subject == 'math'
+  	end
+  	return result
   end
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,4 +1,5 @@
 class Teacher < ApplicationRecord
 	belongs_to :school
 	has_many :student_teacher_pairs
+	has_many :students, through: :student_teacher_pairs
 end
