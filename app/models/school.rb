059e9f8f2231700b1cdd4cfa459d4723df24be4c
Kyle Qian <cqian@stanford.edu>
2016-06-28 14:45:33 -0700

    Creating constants, class methods, and instance methods

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,4 +1,13 @@
 class School < ApplicationRecord
-	has_many :teachers
-	has_many :students
+  has_many :teachers
+  has_many :students
+
+  CREDITS_FOR_GRADUATION = 180
+
+  def self.credits_for_standing(class_standing)
+    if class_standing.between?(1, 4)
+      return CREDITS_FOR_GRADUATION / class_standing # evenly spaced
+    end
+    return nil # not a valid class standing
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,9 @@
 class Student < ApplicationRecord
-	belongs_to :school
-	has_many :student_teacher_pairs
-	has_one :student_record
+  belongs_to :school
+  has_many :student_teacher_pairs
+  has_one :student_record
+
+  def can_graduate
+    return self.tudent_record.credits >= School::CREDITS_FOR_GRADUATION
+  end
 end
