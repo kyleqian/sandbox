894f4383ede7dcebff4e3c57604b4a79f242ccd1
Kyle Qian <cqian@stanford.edu>
2016-07-06 10:42:14 -0700

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+school = School.create!(name: "School name")
+
+teacher = school.teachers.create!(name: "Teacher name", subject: "Teacher subject")
+student = school.students.create!(name: "Student name", year: 4)
+
+student2 = teacher.students.create!(name: "Student2 name", year: 3, school: school)
+teacher2 = student.teachers.create!(name: "Teacher2 name", subject: "Teacher2 subject", school: school)
+
+student_record = StudentRecord.create!(comments: "Comments", date_started: Time.now, credits: 170, student: student)
